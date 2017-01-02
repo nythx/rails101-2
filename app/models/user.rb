09ac2c8b58c9fc014b0797ef7fc669fe879cd255
mavel <69673480@qq.com>
2017-01-02 22:59:40 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
            :recoverable, :rememberable, :trackable, :validatable
 
     has_many :groups
+    has_many :participated_groups, :through => :group_relationships, :source => :group
     has_many :posts
   end
